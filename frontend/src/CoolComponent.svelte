968aba353c25594ff4e0b4821a35c59c372791ac
tobloef <tobloef@gmail.com>
2021-06-24 19:31:19 +0200

    Got Svelte component NodeView to work

---
+++ frontend/src/CoolComponent.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let number;
+</script>
+
+<div id="wrapper">
+    <span>Number: {number}</span>
+</div>
+
+<style>
+    #wrapper {
+        background: dodgerblue;
+    }
+</style>
--- frontend/src/Editor.svelte
+++ frontend/src/Editor.svelte
@@ -1,90 +1,117 @@
-<script>
-  import { onMount } from "svelte";
-  import { EditorState } from "prosemirror-state";
-  import { EditorView } from "prosemirror-view";
-  import { schema } from "prosemirror-schema-basic";
-  import { exampleSetup, buildMenuItems } from "prosemirror-example-setup";
+<script lang="typescript">
+  import {onMount} from "svelte";
+  import {EditorState} from "prosemirror-state";
+  import {EditorView} from "prosemirror-view";
+  import {schema} from "prosemirror-schema-basic";
+  // noinspection TypeScriptCheckImport
+  import {exampleSetup, buildMenuItems} from "prosemirror-example-setup";
   import "prosemirror-menu/style/menu.css";
-  import { Schema } from 'prosemirror-model';
+  import {Schema} from "prosemirror-model";
   import {MenuItem} from "prosemirror-menu"
+  import OrderedMap = require("orderedmap");
+  import CoolComponent from "./CoolComponent.svelte";
 
-  const testNodeSpec = {
-    attrs: { isChecked: { default: false } },
+  const coolComponentSpec = {
+    attrs: {number: {default: 30}},
     inline: true,
     group: "inline",
     draggable: true,
     toDOM: node => [
-      "input",
+      "coolComponent",
       {
-        type: "checkbox",
-        class: "testNode",
-        checked: node.attrs.isChecked ? "" : undefined
+        number: node.attrs.number
       }
     ],
     parseDOM: [
       {
-        tag: "input[type=checkbox]",
+        tag: "coolComponent",
         getAttrs: dom => ({
-          isChecked: dom.getAttribute("isChecked")
+          number: dom.getAttribute("number")
         })
       }
     ]
   };
 
-  const testSchema = new Schema({
-    nodes: schema.spec.nodes.addBefore("image", "test", testNodeSpec),
+  const coolComponentSchema = new Schema({
+    nodes: (schema.spec.nodes as OrderedMap).addBefore("image", "coolComponent", coolComponentSpec),
     marks: schema.spec.marks
   });
 
-  let test = testSchema.nodes.test;
+  let coolComponent = coolComponentSchema.nodes.coolComponent;
 
-  function insertTest(isChecked) {
-    return function(state, dispatch) {
-      let { $from } = state.selection,
-        index = $from.index();
-      if (!$from.parent.canReplaceWith(index, index, test)) {
+  const insertCoolComponent = (number) => {
+    return (state, dispatch) => {
+      let {$from: from} = state.selection,
+        index = from.index();
+      if (!from.parent.canReplaceWith(index, index, coolComponent)) {
         return false;
       }
       if (dispatch) {
-        dispatch(state.tr.replaceSelectionWith(test.create({ isChecked })));
+        dispatch(state.tr.replaceSelectionWith(coolComponent.create({
+          number
+        })));
       }
       return true;
     };
-  }
+  };
 
-  let menu = buildMenuItems(testSchema);
-  [false, true].forEach(isChecked => {
-    const name = `Test (${isChecked ? "Checked" : "Unchecked"})`;
-    return menu.insertMenu.content.push(
-      new MenuItem({
-        title: `Insert ${name}`,
-        label: name,
-        enable(state) {
-          return insertTest(isChecked)(state);
-        },
-        run: insertTest(isChecked)
-      })
-    );
-  });
+  let menu = buildMenuItems(coolComponentSchema);
+  menu.insertMenu.content.push(
+    new MenuItem({
+      title: `Insert 10`,
+      label: `Cool component (10)`,
+      enable(state) {
+        return insertCoolComponent(10)(state);
+      },
+      run: insertCoolComponent(10)
+    })
+  );
+  menu.insertMenu.content.push(
+    new MenuItem({
+      title: `Insert 20`,
+      label: `Cool component (20)`,
+      enable(state) {
+        return insertCoolComponent(20)(state);
+      },
+      run: insertCoolComponent(20)
+    })
+  );
+
+  class CoolComponentView {
+    dom;
+    component;
+
+    constructor(node) {
+      console.log("node", node);
+      this.dom = document.createElement("div");
+      this.component = new CoolComponent({
+        target: this.dom,
+        props: {
+          number: node.attrs.number
+        }
+      });
+    }
+  }
 
   onMount(() => {
     const editorElement = document.querySelector("#editor");
     const state = EditorState.create({
-      schema: testSchema,
+      schema: coolComponentSchema,
       plugins: exampleSetup({
-	      schema: testSchema,
-	      menuContent: menu.fullMenu,
+        schema: coolComponentSchema,
+        menuContent: menu.fullMenu,
       }),
     });
     const view = new EditorView(editorElement, {
       state,
+      nodeViews: {
+        coolComponent: (node) => new CoolComponentView(node),
+      }
     });
   });
 </script>
 
-<div id="editor">
-
-</div>
+<div id="editor"></div>
 
 <style>
   #editor {
@@ -110,5 +137,9 @@
     flex: 1;
     overflow: auto;
     min-height: 400px;
+  }
+
+  #hidden-components {
+      display: none;
   }
 </style>
